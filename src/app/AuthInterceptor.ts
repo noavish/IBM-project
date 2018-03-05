cb786266aa8ae7c77dc6f1f5d91c75703668050c
Dor <simhondor@gmail.com>
2018-03-05 10:47:07 +0200

    not done

--- src/app/AuthInterceptor.ts
+++ src/app/AuthInterceptor.ts
@@ -12,15 +12,12 @@ export class AuthInterceptor implements HttpInterceptor {
 
     const idToken = localStorage.getItem('token');
 
-    if (idToken) {
       const cloned = req.clone({
         headers: req.headers.set('Authorization',
           'Bearer ' + idToken)
       });
 
       return next.handle(cloned);
-    } else {
-      return next.handle(req);
-    }
+
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,18 +15,6 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  // $("#btnLogin").click(function(event) {
-
-  //   //Fetch form to apply custom Bootstrap validation
-  //   var form = $("#formLogin")
-
-  //   if (form[0].checkValidity() === false) {
-  //     event.preventDefault()
-  //     event.stopPropagation()
-  //   }
-
-  //   form.addClass('was-validated');
-  // });
 
     login() {
     const user = {username: this.username, password: this.password};
